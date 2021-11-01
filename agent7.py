33dc4326069429dc7b66f32567917efe50578b31
GunjanAS <gs63@njit.edu>
2021-11-01 17:33:30 -0400

    till step 5

--- agent7.py
+++ agent7.py
@@ -8,6 +8,7 @@ class Agent7():
         self.start_cell,self.target_cell=self.set_start_and_target_cell()
         print("Start cell is" ,self.start_cell)
         print("target cell is" ,self.target_cell)
+        self.terrain_fnr_dict={1:0.8,2:0.5,3:0.2}
         self.belief_matrix = np.full((self.dim, self.dim), 1/(self.dim**2))
         self.success_finding_matrix=np.full((self.dim, self.dim), 1/(self.dim**2))
 
@@ -52,4 +53,75 @@ class Agent7():
                 print(e, end=" ")
             print()
 
-a= Agent7(8)
+def get_fnr(a7obj,cell):
+    grid_val=a7obj.original_grid[cell[0]][cell[1]]
+    return a7obj.terrain_fnr_dict[grid_val]
+
+def examine_current_cell(a7obj):
+
+    current_cell=a7obj.start_cell
+    if current_cell== a7obj.target_cell:
+        p = random.uniform(0, 1)
+        currentcell_fnr=get_fnr(a7obj,current_cell)
+        if p>currentcell_fnr:
+            print("Found the target and exiting")
+            return 1 #exit
+        else:
+            return 2 #todo
+    return 3 #couldm't find, decrease prob
+    
+def get_closest_cell(curr_cell,listOfCoordinates) :
+    min_dist=float('inf')
+    closest_cell_loc=[]
+    for cord in listOfCoordinates:
+        dist=abs(cord[0]-curr_cell[0])+abs(cord[1]-curr_cell[1])
+        if dist<=min_dist:
+            min_dist=dist
+            closest_cell_loc.append(cord)
+    finalclosest_cell_loc=[] 
+    for cellpos in closest_cell_loc:
+        if abs(cellpos[0]-curr_cell[0])+abs(cellpos[1]-curr_cell[1])==min_dist:
+            finalclosest_cell_loc.append(cellpos)
+
+
+
+    return finalclosest_cell_loc
+
+
+
+
+def get_max_probcell(curr_cell,success_finding_matrix):
+    maxval=np.max(success_finding_matrix)
+    locations=np.where(success_finding_matrix==maxval)
+    listOfCoordinates= list(zip(locations[0], locations[1]))
+    if len(listOfCoordinates)>1:
+        closest_cell_list=get_closest_cell(curr_cell,listOfCoordinates)
+        print("closest_cell_list",closest_cell_list)
+        if len(closest_cell_list)>1:
+            return random.choice(closest_cell_list)
+    return listOfCoordinates
+
+
+
+def main_a7():
+    a7obj= Agent7(5)
+    curr_cell=a7obj.start_cell
+    if examine_current_cell(a7obj)==1:
+        return
+    elif examine_current_cell(a7obj)==3:
+        a7obj.belief_matrix[a7obj.start_cell[0]][a7obj.start_cell[1]]*= get_fnr(a7obj,a7obj.start_cell)
+        belief_sum = np.sum(a7obj.belief_matrix)
+        a7obj.belief_matrix = a7obj.belief_matrix/belief_sum
+        
+        a7obj.success_finding_matrix[a7obj.start_cell[0]][a7obj.start_cell[1]]=a7obj.belief_matrix[a7obj.start_cell[0]][a7obj.start_cell[1]]*(1-get_fnr(a7obj,a7obj.start_cell))
+        conf_sum = np.sum(a7obj.success_finding_matrix)
+        a7obj.success_finding_matrix = a7obj.success_finding_matrix/conf_sum
+    print(a7obj.belief_matrix)
+    print(a7obj.success_finding_matrix)
+    
+    next_target_cell=get_max_probcell(curr_cell,a7obj.success_finding_matrix)
+    print(next_target_cell)
+
+
+
+main_a7()

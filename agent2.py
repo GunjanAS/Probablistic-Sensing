5ddfa2ba8e4ba50949e148731557d28c33a65f14
GunjanAS <gs63@njit.edu>
2021-11-16 02:06:59 -0500

    a2 remove spaces

--- agent2.py
+++ agent2.py
@@ -59,7 +59,6 @@ def generate_children(grid, knowledge_grid, fringe, visited_list, current, all_m
     current_x, current_y = current.position
     relevant_children = []
     dim = len(grid)
-    # print("Current:- "  + str(current.position[0]) + " " + str(current.position[1]))
     for a_move in all_moves:
         child_x = current_x + a_move[0]
         child_y = current_y + a_move[1]
@@ -73,7 +72,6 @@ def generate_children(grid, knowledge_grid, fringe, visited_list, current, all_m
             grid_for_pathcalculation = grid
         if (grid_for_pathcalculation[child_x][child_y] != 0) and (visited_list.get(children_node.position) != "Added"):
             children_node.g = current.g + 1
-            
             children_node.h = abs(children_node.position[0] - end_node.position[0]) + abs(
                     children_node.position[1] - end_node.position[1])
             children_node.f = children_node.g + children_node.h
@@ -84,13 +82,10 @@ def generate_children(grid, knowledge_grid, fringe, visited_list, current, all_m
 def search(numberofcellsprocessed,grid, fringe, knowledge_grid, start_position, end_position, is_gridknown,):
     startNode = Node(None, start_position)
     endNode = Node(None, end_position)
-
-    # fringe = PriorityQueue()
     fringe = []
     visited_nodes = {}
     already_fringed = {}
     already_fringed[startNode.position] = startNode.f
-    # fringe.insert(startNode)
     heapq.heappush(fringe, (startNode.f, startNode))
     all_moves = [[1, 0],
                  [0, 1],
@@ -101,7 +96,6 @@ def search(numberofcellsprocessed,grid, fringe, knowledge_grid, start_position, 
         current = heapq.heappop(fringe)
         numberofcellsprocessed+=1
         current = current[1]
-        # print("current",current.position)
         visited_nodes[current.position] = "Added"
         if current.position == endNode.position:
 
@@ -132,16 +126,7 @@ def search(numberofcellsprocessed,grid, fringe, knowledge_grid, start_position, 
 
 def main(dim,is_gridknown,grid,knowledge_grid,start,end):
     fringe = []
-    # knowledge_grid = [[1 for _ in range(dim)] for _ in range(dim)]
     bumps=0
-
-    # pltGrid = deepcopy(grid)
-    # pltGrid[0][0] = 3
-    # prevPos = [0,0]
-    # pyplot.figure(figsize=(20, 20))
-    # pyplot.grid()
-    # im = None
-    # print_grid(grid)
     numberofcellsprocessed=0
     
     all_moves = [[1, 0],
@@ -153,65 +138,6 @@ def main(dim,is_gridknown,grid,knowledge_grid,start,end):
     ll, path,numberofcellsprocessed = search(numberofcellsprocessed,grid, fringe, knowledge_grid,
                                                  start, end, is_gridknown)
     return path,knowledge_grid
-    final_path = []
-    if(ll != "Unsolvable" and is_gridknown == "No"):
-        print("is_gridknown",is_gridknown)
-        while(len(path) > 1 and ll!="Unsolvable"):
-            count = 0
-            flag = 0
-            # traverse the path obtained from search function to see if blocked cell exists or not.
-            # If blocked cell exists, run search function again to calculate the path
-            # Continue in this while loop -1) either path returned is 0 that means nothing left in fringe and no path to reach goal 2) or path exists to reach goal
-            for i in path[::-1]:
-                count += 1
-                
-                final_path.append((i[0], i[1]))
-                if(grid[i[0]][i[1]] == 0):  # blocked in grid
-                    bumps+=1
-                    final_path.pop()
-
-                    # updating knowledge_grid
-                    knowledge_grid[i[0]][i[1]] = 0
-                    new_start_position = path[path.index(
-                        i)+1][0], path[path.index(i)+1][1]
-
-                    ll, path,numberofcellsprocessed = search(numberofcellsprocessed,grid, fringe, knowledge_grid,
-                                                                 new_start_position, end, is_gridknown, )
-                    finalresult = ll
-                    break
-                elif(grid[i[0]][i[1]] == 1):
-                    knowledge_grid[i[0]][i[1]] = 2
-                # pltGrid[prevPos[0]][prevPos[1]] = 2
-                # pltGrid[i[0]][i[1]] = 3
-                # prevPos = [i[0], i[1]]
-                # # pyplot.imshow(pltGrid)
-                # if (im is None) :
-                #     im = pyplot.imshow(pltGrid)
-                # else:
-                #     im.set_data(pltGrid)
-                # pyplot.pause(0.5)
-
-                if(count == len(path)):
-                    print("Solved")
-                    flag = 1
-                # print("final_path",final_path)
-                    break
-            if(flag == 1):
-                return final_path, knowledge_grid,bumps,numberofcellsprocessed
-                break
-        if(ll=="Unsolvable"):
-            return [],knowledge_grid,bumps,numberofcellsprocessed
-        if(flag != 1):
-            print("finalresult", finalresult)
-            return [],knowledge_grid,bumps,numberofcellsprocessed
-    elif(is_gridknown == "Yes"):
-        print(ll)
-        return path
-        # print("path",path)
-    else:
-        print("Unsolvable")
-        return [],knowledge_grid,bumps,numberofcellsprocessed
-    # pyplot.show()
 
     
     
